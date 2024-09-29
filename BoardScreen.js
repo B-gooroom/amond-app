41cd72eddd34b4838a652301582b3104da61c373
sean <j.sean1316@gmail.com>
2024-09-30 00:29:54 +0900

    FEAT: 페이지별 스크린추가

---
+++ BoardScreen.js
@@ -0,0 +1,34 @@
+import React, { useRef } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function BoardScreen() {
+  const webviewRef = useRef(null);
+
+  const onWebViewLoad = () => {
+    // 웹뷰 로드 후 웹 탭바를 숨기기 위해 메시지 전송
+    webviewRef.current.postMessage("native");
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        ref={webviewRef}
+        onLoad={onWebViewLoad}
+        // source={{ uri: "https://amond.kr/Board" }}
+        source={{ uri: "http://192.168.35.253:3000/Board" }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  webview: {
+    flex: 1,
+  },
+});
